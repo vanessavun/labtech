de51bf0f52d0e078fb8e6c1d3896b123ab4276f1
Vanessa Vun <vanessavun@gmail.com>
2022-10-14 17:47:01 -0700

    Created handlechange for search bar

--- src/components/data-card-dropdown/data-card-dropdown.component.jsx
+++ src/components/data-card-dropdown/data-card-dropdown.component.jsx
@@ -1,40 +1,39 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react'
 import { KanbanContext } from '../../context/KanbanContext'
 
-function DataCardDropdown({data}) {
+function DataCardDropdown({ data }) {
     console.log("Datacard dropdown data:", data)
-    const {moveToNextTest} = useContext(KanbanContext);
+    const { moveToNextTest } = useContext(KanbanContext);
 
     return (
         <>
             <button type="button" className="btn btn-secondary dropdown-toggle btn-sm" data-bs-toggle="dropdown" aria-expanded="false" >
             </button>
             <ul className="dropdown-menu">
-            <li>
-                <button 
-                className="dropdown-item" 
-                onClick={()=>console.log("START")}>
-                Start run
-                </button>
-            </li>
-            <li>
-                <button 
-                    className="dropdown-item" 
-                    onClick={()=>moveToNextTest(data)}>
-                    {data.test !== 'sequencing' ? "Proceed next step" : "Remove"}
-                </button>
-            </li>
-            {/* Future functions:
-            <li><hr className='dropdown-divider' /></li>
-            <li><button className="dropdown-item" onClick={()=>console.log("FAIL")}>Fail run</button></li>
-            <li><button className="dropdown-item" onClick={()=>console.log("RERUN")}>Rerun batch</button></li>
-            <li><button className="dropdown-item" onClick={()=>console.log("RE-EXTRACT")}>Re-extract</button></li>
-            <li><hr className='dropdown-divider' /></li>
-            <li><button className="dropdown-item" onClick={()=>console.log("BATCH INFO")}>Batch information</button></li> 
-            */}
+                <li>
+                    <button
+                        className="dropdown-item"
+                        onClick={() => console.log("START")}
+                        disabled>
+                        Start run
+                    </button>
+                </li>
+                <li>
+                    <button
+                        className="dropdown-item"
+                        onClick={() => moveToNextTest(data)}>
+                        {data.test !== 'sequencing' ? "Proceed next step" : "Remove"}
+                    </button>
+                </li>
+                <li><hr className='dropdown-divider' /></li>
+                <li><button className="dropdown-item" onClick={() => console.log("FAIL")} disabled>Fail run</button></li>
+                <li><button className="dropdown-item" onClick={() => console.log("RERUN")}disabled>Rerun batch</button></li>
+                <li><button className="dropdown-item" onClick={() => console.log("RE-EXTRACT")} disabled>Re-extract</button></li>
+                <li><hr className='dropdown-divider' /></li>
+                <li><button className="dropdown-item" onClick={() => console.log("BATCH INFO")} disabled>Batch information</button></li>
             </ul>
         </>
-  )
+    )
 }
 
 export default DataCardDropdown
--- src/components/kanban-header/kanban-header.component.jsx
+++ src/components/kanban-header/kanban-header.component.jsx
@@ -1,9 +1,15 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { KanbanContext } from '../../context/KanbanContext'
 
 function KanbanHeader() {
   const {addBatch} = useContext(KanbanContext)
-  
+  const [searchInput, setSearchInput] = useState({search: ""})
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+    setSearchInput(prevInput => ({...prevInput, [name]: value}))
+  }
+  console.log(searchInput)
+
   return (
     <div className='d-flex justify-content-around m-2'>
         <button 
@@ -17,10 +23,17 @@ function KanbanHeader() {
               className="form-control me-2" 
               type="search" 
               placeholder="Search" 
-              aria-label="Search" />
+              aria-label="Search"
+              name="search"
+              value={searchInput.search}
+              onChange={handleChange}
+            />
             <button 
               className="btn btn-outline-success" 
-              type="submit">Search
+              type="button"
+              onClick={() => console.log("Search button clicked")}
+            >
+              Search
             </button>
         </form>
     </div>
